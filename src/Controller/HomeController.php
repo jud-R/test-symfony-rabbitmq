d9bba8b720d2ed9d399afc8d1c946e796deccccc
jud <r.judicael@gmail.com>
2021-01-19 22:46:51 +0100

    simulate slow email sending + add docker-compose mailCatcher

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use DateTime;
 use App\Entity\Incident;
 use App\Form\IncidentType;
+use Symfony\Component\Mime\Email;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,7 +18,8 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index(Request $request, EntityManagerInterface $em): Response
+    public function index(Request $request, EntityManagerInterface $em,
+    MailerInterface $mailer): Response
     {
         $task = new Incident();
         $task->setUser($this->getUser());
@@ -29,8 +32,20 @@ class HomeController extends AbstractController
             $task = $form->getData();
 
             $em->persist($task);
-
             $em->flush();
+
+            $email = (new Email())
+                ->from($task->getUser()->getEmail())
+                ->to('user@test.test')
+                ->subject("New Incident #{$task->getId()} - {$task->getUser()->getEmail()}")
+                ->html("<p>{$task->getDescription()}</p>");
+                
+
+                // simulate slow process
+                sleep(8);
+
+                $mailer->send($email);
+
 
             return $this->redirectToRoute('home');
         }
